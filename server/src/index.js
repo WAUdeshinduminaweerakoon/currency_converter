867ea02884c97208e6bbc1476fc2e2256f24e656
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2024-04-28 10:47:40 +0530

    server side adding the express,cors,axios and listen to a port

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+//middle wares
+app.use(express.json());
+app.use(cors());
+
+//listen to a port
+app.listen(5000, ()=>{
+    console.log("SERVER STARTED");
+})
